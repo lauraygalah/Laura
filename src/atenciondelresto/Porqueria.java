5ae0af388fbc26bc01fc28a80783748ff9b2370c
Pamela Iriarte <iriarte.pamelama@gmail.com>
2018-10-08 20:11:09 -0300

    clase prueba

---
+++ src/atenciondelresto/Porqueria.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atenciondelresto;
+
+/**
+ *
+ * @author Jofre
+ */
+public class Porqueria {
+    private int algo;
+    private String dale = "mierda sali!!!!!";
+    
+}
